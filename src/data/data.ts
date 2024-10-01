3553a9a725ccec3674bf8747aff7521b151c4724
varun_everest <varunkumar.martha@everest.engineering>
2024-10-01 12:38:26 +0530

    Modify static data

--- src/data/data.ts
+++ src/data/data.ts
@@ -6,14 +6,27 @@ export const users : TUser[] = [
         password:'V@run765',
         salary: 25000,
         otherIncome: 50000,
-        transactions: []
+        transactions: [],
+        expenses: [
+            {
+                category: 'Groceries',
+                allotedAmount: 10000,
+                usedAmount: 500,
+            },
+            {
+                category: 'Food',
+                allotedAmount: 1000,
+                usedAmount: 300,
+            },
+        ],
     }, 
     {
         username:'Virat',
         password:'Virat@18',
         salary: 30000,
         otherIncome: 10000,
-        transactions: []
+        transactions: [],
+        expenses: [],
     }
 ]
 
