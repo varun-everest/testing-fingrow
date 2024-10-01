ed1581419c3c729c19b36c1d051b8dd0fb1a0110
varun_everest <varunkumar.martha@everest.engineering>
2024-10-01 10:49:04 +0530

    Add sample static data

---
+++ src/data/data.ts
@@ -0,0 +1,16 @@
+import { TUser } from "../types/types";
+
+export const users : TUser[] = [
+    {
+        username:'Varun',
+        password:'V@run765',
+        salary: 25000,
+        otherIncome: 50000,
+    }, 
+    {
+        username:'Virat',
+        password:'Virat@18',
+        salary: 30000,
+        otherIncome: 10000
+    }
+]
