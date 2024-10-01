e294fff84c4fd35ca767bc6cf1fe8fadeb16be86
varun_everest <varunkumar.martha@everest.engineering>
2024-10-01 11:43:17 +0530

    Add TTransaction type

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,9 +3,16 @@ export type TIncome = {
     amount: number
 }
 
+export type TTransaction = {
+    transactionType : string,
+    amount: number,
+    date : Date,
+}
+
 export type TUser = {
     username: string,
     password: string,
+    transactions: TTransaction[]
     salary: number,
     otherIncome: number
 }
