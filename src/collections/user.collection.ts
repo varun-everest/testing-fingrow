5ab79a63042e736d5838a34571af88e0da14b6cd
varun_everest <varunkumar.martha@everest.engineering>
2024-10-03 15:00:25 +0530

    Create User Collection

---
+++ src/collections/user.collection.ts
@@ -0,0 +1,23 @@
+import mongoose, {Schema} from "mongoose";
+import TIncome from "../types/TIncome";
+
+const incomeSchema = new Schema<TIncome>({
+    source: String,
+    amount: Number,
+})
+
+const userSchema = new Schema({
+    username : String,
+    password : String,
+    totalIncome :  [
+        { 
+            type : incomeSchema
+        }
+    ],
+    amountUsed : Number
+});
+
+const UserModel = mongoose.model('Users', userSchema);
+console.log("Successfully created Users Collections");
+
+export default UserModel;
