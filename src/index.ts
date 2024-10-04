15d1eb1b279704e9074fdfa904cd663a07849728
varun_everest <varunkumar.martha@everest.engineering>
2024-10-04 10:20:15 +0530

    Start express server

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import startDatabase from './database/connection';
+import UserModel from './collections/user.collection';
+import TransactionModel from './collections/transactions.collection';
+import BudgetModel from './collections/budget.collection';
+import SavingGoalsModel from './collections/savingsgoals.collection';
+import router from './routes/router';
+
+
+const PORT = 4000;
+const app = express();
+
+const initializeDatabase = async() => {
+    await startDatabase();
+    // UserModel
+    // TransactionModel
+    // BudgetModel;
+    // SavingGoalsModel;
+}
+
+initializeDatabase();
+
+app.use('/api', router);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http:localhost:${PORT}/`);
+});
