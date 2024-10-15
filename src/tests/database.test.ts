969de5429f0e363de5ae7559b7026ae620df2192
varun_everest <varunkumar.martha@everest.engineering>
2024-10-15 11:27:00 +0530

    Add test case that fails database connection

---
+++ src/tests/database.test.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import startDatabase from '../database/connection'; 
+
+jest.mock('mongoose', () => ({
+    connect: jest.fn(),
+}));
+
+describe('Tests related to Database Connection', () => {
+    const originalConsoleLog = console.log; 
+
+    beforeEach(() => {
+        console.log = jest.fn(); 
+    });
+
+    afterEach(() => {
+        console.log = originalConsoleLog; 
+        jest.clearAllMocks(); 
+    });
+
+    test('should give an error if connection fails', async () => {
+        const errorMessage = 'Connection error';
+        (mongoose.connect as jest.Mock).mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
+
+        await startDatabase();
+
+        expect(mongoose.connect).toHaveBeenCalledWith('mongodb://127.0.0.1:27017/fingrow');
+        expect(console.log).toHaveBeenCalledWith("Error Occurred : ", expect.any(Error));
+    });
+});
