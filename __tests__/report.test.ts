d9362c7d523209959ad8e01590cb41369b0fbfef
varun_everest <varunkumar.martha@everest.engineering>
2024-10-15 19:27:18 +0530

    Add test case to report for user does not exist

---
+++ __tests__/report.test.ts
@@ -0,0 +1,37 @@
+import User from '../src/classes/User';
+import UserModel from '../src/collections/user.collection';
+import TransactionModel from '../src/collections/transactions.collection';
+import BudgetModel from '../src/collections/budget.collection';
+import SavingGoalsModel from '../src/collections/savingsgoals.collection';
+
+jest.mock('../src/collections/user.collection');
+jest.mock('../src/collections/transactions.collection');
+jest.mock('../src/collections/budget.collection');
+jest.mock('../src/collections/savingsgoals.collection');
+
+describe('Tests related to generating report', () => {
+    const username = 'varun';
+    const password = 'V@run765';
+    const totalIncome = 5000;
+    let user: User;
+
+    beforeEach(() => {
+        user = new User(username, password, totalIncome);
+        jest.clearAllMocks();
+        console.log = jest.fn(); 
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    test('should give an error if the user does not exist', async () => {
+        (UserModel.findOne as jest.Mock).mockResolvedValueOnce(null);
+
+        await user.generateReport('total', new Date('2023-01-01'), new Date('2023-12-31'));
+
+        expect(console.log).toHaveBeenCalledWith("User does not exist");
+    });
+
+    
+});
