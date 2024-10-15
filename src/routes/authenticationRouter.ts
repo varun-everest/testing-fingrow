707014caa801bfdacfb6e5f6cd5c8ddaaf989b2b
varun_everest <varunkumar.martha@everest.engineering>
2024-10-15 21:15:36 +0530

    Implement auth routes

---
+++ src/routes/authenticationRouter.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from 'express'
+import UserModel from '../collections/user.collection';
+
+const authRouter = express.Router();
+authRouter.use(express.json());
+
+//Recieves username and password!!
+authRouter.get('/login', async(req: Request, res: Response) => {
+    
+    const findUser = await UserModel.findOne({username: req.body.username});
+    if(!findUser) {
+        res.send('Username does not exists!!');
+        return;
+    }
+    if(findUser.password !== req.body.password) {
+        res.send('Incorrect Password');
+        return;
+    }
+    res.send(`Login successful`);
+});
+
+//It regiseters a new user with unique username...
+authRouter.post('/register', async(req: Request, res: Response) => {
+    try{
+        const findUser = await UserModel.findOne({username: req.body.username});
+        if(findUser) {
+            res.send('UserName already exists');
+            return;
+        }
+        const user = await UserModel.create(req.body);
+        await user.save();
+        res.send(user);
+    } catch(err) {
+        console.log("Error Occurred : ", err);
+        res.status(400).send('BadRequest');
+    }
+});
+
+export default authRouter;
