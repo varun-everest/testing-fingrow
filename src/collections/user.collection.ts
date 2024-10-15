3c84e209c6d16dd1873e5d4864b8d95b142ab642
varun_everest <varunkumar.martha@everest.engineering>
2024-10-15 12:55:47 +0530

    Remove comment statements

--- src/collections/user.collection.ts
+++ src/collections/user.collection.ts
@@ -1,10 +1,4 @@
 import mongoose, {Schema} from "mongoose";
-// import TIncome from "../types/TIncome";
-
-/* const incomeSchema = new Schema<TIncome>({
-    source: String,
-    amount: Number,
-}); */
 
 const userSchema = new Schema({
     username : {
