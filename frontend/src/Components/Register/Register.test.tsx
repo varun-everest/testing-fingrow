ff63516488f343bf71abc860c4c1a001fe153c60
varun_everest <varunkumar.martha@everest.engineering>
2024-10-17 03:00:40 +0530

    Add test case that navigate to login component

--- frontend/src/Components/Register/Register.test.tsx
+++ frontend/src/Components/Register/Register.test.tsx
@@ -1,18 +1,28 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Register from "./Register"
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import Login from '../Login/Login';
 
 
 describe('Tests related to Register component', () => {
 
     test('should renders the Register heading', () => {
-        render(<Register />);
+        render(
+            <MemoryRouter>
+                <Register />
+            </MemoryRouter>
+        );
 
         const register = screen.getByText('Register!')
         expect(register).toBeInTheDocument()
     })
 
     test('should renders the username field', () => {
-        render(<Register />)
+        render(
+            <MemoryRouter>
+                <Register />
+            </MemoryRouter>
+        );
 
         const usernameElement = screen.getByLabelText(/username/i);
         expect(usernameElement).toBeInTheDocument();
@@ -20,7 +30,11 @@ describe('Tests related to Register component', () => {
     });
 
     test('should renders the password field', () => {
-        render(<Register />)
+        render(
+            <MemoryRouter>
+                <Register />
+            </MemoryRouter>
+        );
 
         const passwordElement = screen.getByLabelText(/password/i);
         expect(passwordElement).toBeInTheDocument();
@@ -28,7 +42,11 @@ describe('Tests related to Register component', () => {
     });
 
     test('should renders the total income field', () => {
-        render(<Register />)
+        render(
+            <MemoryRouter>
+                <Register />
+            </MemoryRouter>
+        );
 
         const totalIncomeElement = screen.getByLabelText(/total income/i);
         expect(totalIncomeElement).toBeInTheDocument();
@@ -36,7 +54,11 @@ describe('Tests related to Register component', () => {
     });
 
     test('should renders the register button', () => {
-        render(<Register />)
+        render(
+            <MemoryRouter>
+                <Register />
+            </MemoryRouter>
+        );
 
         const registerButton = screen.getByRole("button", {
             name: 'Register'
@@ -45,12 +67,33 @@ describe('Tests related to Register component', () => {
     });
 
     test('should renders the already have an account text and login here text', () => {
-        render(<Register />)
+        render(
+            <MemoryRouter>
+                <Register />
+            </MemoryRouter>
+        );
 
         const textElement = screen.getByText('Already have an account ?');
         const loginTextElement = screen.getByText('Login here');
 
         expect(textElement).toBeInTheDocument();
         expect(loginTextElement).toBeInTheDocument();
-    })
+    });
+
+    test('should navigate to the Login component when login link is clicked', () => {
+        render(
+            <MemoryRouter initialEntries={['/register']}> 
+                <Register />
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </MemoryRouter>
+        );
+
+        const loginLink = screen.getByText('Login here');
+        fireEvent.click(loginLink);
+
+        const loginHeading = screen.getByText('Login!'); 
+        expect(loginHeading).toBeInTheDocument();
+    });
 })
