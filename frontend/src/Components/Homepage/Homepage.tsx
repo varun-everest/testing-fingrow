852495ae70487cb06a7da63793b306e9eaebf2a6
varun_everest <varunkumar.martha@everest.engineering>
2024-10-16 23:53:09 +0530

    Add className for elements

--- frontend/src/Components/Homepage/Homepage.tsx
+++ frontend/src/Components/Homepage/Homepage.tsx
@@ -1,10 +1,15 @@
+import './Homepage.css'
 
 const Homepage = () => {
   return (
-    <div>
-        <h1>Welcome to FinGrow Finance Tracker Application</h1>
-        <button>Login</button>
-        <button>Register</button>
+    <div className='container'>
+        <h1 className='title'>Welcome to FinGrow Finance Tracker Application</h1>
+        <h3 className='tag-title'>Smart Tracking for Smarter Transactions</h3>
+        <div>
+            <button className='button'>Login</button>
+            <button className='button'>Register</button>
+        </div>
+        
     </div>
   )
 }
