483997b7b2eb30bed0ad4171b6da45caaa34e819
varun_everest <varunkumar.martha@everest.engineering>
2024-10-17 02:12:46 +0530

    Add Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './Components/Homepage/Homepage';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
 
 
 function App() {
   return (
-    <div>
-      Hello!!. This is react project
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </>
   );
 }
 
