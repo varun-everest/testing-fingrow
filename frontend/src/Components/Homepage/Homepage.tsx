0ebffa2c10f6d519dce6383536c0b98d43c300e5
varun_everest <varunkumar.martha@everest.engineering>
2024-10-17 02:11:26 +0530

    Rename button to Hbutton

--- frontend/src/Components/Homepage/Homepage.tsx
+++ frontend/src/Components/Homepage/Homepage.tsx
@@ -6,8 +6,8 @@ const Homepage = () => {
         <h1 className='title'>Welcome to FinGrow Finance Tracker Application</h1>
         <h3 className='tag-title'>Smart Tracking for Smarter Transactions</h3>
         <div>
-            <button className='button'>Login</button>
-            <button className='button'>Register</button>
+            <button className='Hbutton'>Login</button>
+            <button className='Hbutton'>Register</button>
         </div>
         
     </div>
