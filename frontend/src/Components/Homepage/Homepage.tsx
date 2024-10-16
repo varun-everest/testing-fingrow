ba5742d0fe4b4de22b12a595373357f5713bd114
varun_everest <varunkumar.martha@everest.engineering>
2024-10-17 03:24:51 +0530

    Add Link component

--- frontend/src/Components/Homepage/Homepage.tsx
+++ frontend/src/Components/Homepage/Homepage.tsx
@@ -1,4 +1,5 @@
-import './Homepage.css'
+import './Homepage.css';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   return (
@@ -6,8 +7,8 @@ const Homepage = () => {
         <h1 className='title'>Welcome to FinGrow Finance Tracker Application</h1>
         <h3 className='tag-title'>Smart Tracking for Smarter Transactions</h3>
         <div>
-            <button className='Hbutton'>Login</button>
-            <button className='Hbutton'>Register</button>
+            <Link to='/login'><button className='Hbutton'>Login</button></Link>
+            <Link to='/register'><button className='Hbutton'>Register</button></Link>
         </div>
         
     </div>
