fa61b7a4cc2aff0fa955e25d6cd3ce7375057fba
varun_everest <varunkumar.martha@everest.engineering>
2024-10-17 16:06:57 +0530

    Add states

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -1,23 +1,35 @@
+import { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        if(username.length === 0 || password.length === 0) {
+            alert('All fields are required!!');
+            return;
+        }
+    }
+
   return (
     <div className='login-container'>
         <div className='login-form-container'>
             <p className='login-heading'>Login!</p>
             <div className='login-field-container'>
                 <label htmlFor='username'>Username</label>
-                <input type="text" id="username" required />
+                <input type="text" id="username" required value={username} onChange={(e) => {setUsername(e.target.value)}} />
             </div>
             <div className='login-field-container'>
                 <label htmlFor='password'>Password</label>
-                <input type="password" id='password' required/>
+                <input type="password" id='password' required value={password} onChange={(e) => {setPassword(e.target.value)}} />
             </div>
             <span>
                 <span>Don't you have an account ? </span> <span className='register-text'><Link to='/register'>Register here</Link></span>
             </span>
-            <button className='login-button'>Login</button>
+            <button className='login-button' onClick={handleLogin}>Login</button>
         </div>
     </div>
   )
