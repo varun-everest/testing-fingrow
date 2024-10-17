668b564d58c0e7c9ade58fc2c116280bd5f68cee
varun_everest <varunkumar.martha@everest.engineering>
2024-10-17 14:50:42 +0530

    Initialize server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,28 +7,37 @@ import SavingGoalsModel from './collections/savingsgoals.collection';
 import router from './routes/router';
 import User from './classes/User';
 import { readCsvFile } from './ImportData/importTransactions';
+import cors from 'cors';
 
 
-// const PORT = 4000;
-// const app = express();
-
+const PORT = 4000;
+const app = express();
+app.use(cors(
+    {
+      origin: 'http://localhost:3000', 
+      methods: 'GET,POST,PUT,DELETE',   
+      credentials: true,   
+      allowedHeaders: ['Content-Type', 'Authorization']             
+    }
+  ))
+  
 const initializeDatabase = async() => {
     await startDatabase();
-    UserModel
-    TransactionModel
-    BudgetModel;
-    SavingGoalsModel;
+    // UserModel
+    // TransactionModel
+    // BudgetModel;
+    // SavingGoalsModel;
 
-    const user = new User('arun','V@run765', 100000);
-    await user.registerUser();
+    // const user = new User('arun','V@run765', 100000);
+    // await user.registerUser();
     // readCsvFile('src/data/previoustransactions.csv')
 }
 
 initializeDatabase();
 
-// app.use('/api', router);
+app.use('/fingrow', router);
 
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running at http:localhost:${PORT}/`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
