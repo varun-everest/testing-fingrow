f424174cc3087479fa8b60b0a676fe64ea63be3b
varun_everest <varunkumar.martha@everest.engineering>
2024-10-23 02:29:29 +0530

    Add test cases for App

--- frontend/src/tests/App.test.tsx
+++ frontend/src/tests/App.test.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
+import { InitialEntry } from '@remix-run/router';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello!!. This is react project/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Tests relate to the App Component', () => {
+
+    const renderAppWithRouter = (initialEntries: InitialEntry[]) => {
+      return render(
+        <MemoryRouter initialEntries={initialEntries}>
+          <App />
+        </MemoryRouter>
+      );
+    };
+
+    test('should renders Homepage component when navigating to /', () => {
+        renderAppWithRouter(['/']); 
+        expect(screen.getByText(/Welcome to FinGrow Finance Tracker Application/i)).toBeInTheDocument(); 
+    });
+
+    test('should renders Register component when navigating to /register', () => {
+        renderAppWithRouter(['/register']);
+        expect(screen.getByText(/Register!/i)).toBeInTheDocument(); 
+    });
+
+    test('should renders Login component when navigating to /login', () => {
+        renderAppWithRouter(['/login']); 
+        expect(screen.getByText(/Login!/i)).toBeInTheDocument(); 
+    });
+
+    test('should renders MainPage component when navigating to /home', () => {
+        renderAppWithRouter(['/home']);
+        expect(screen.getByText(/FinGrow Finance Tracker/i)).toBeInTheDocument(); 
+    });
 });
