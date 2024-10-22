198fa554dda24fe8c7f9e48cb3e7d2175a8e3ad7
varun_everest <varunkumar.martha@everest.engineering>
2024-10-22 23:56:10 +0530

    Add POST method to send Reports

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import UserModel from '../collections/user.collection';
+import Report from '../services/Report';
 
 const userRouter = express.Router();
 userRouter.use(express.json());
@@ -9,5 +10,22 @@ userRouter.get('/users', async(req: Request, res: Response) => {
     res.status(200).send(users);
 });
 
+userRouter.post('/:username/reports', async(req: Request, res: Response) => {
+
+    const {reportType, startDate, endDate} = req.body;
+
+    const report = new Report(reportType, startDate, endDate);
+    const reportStatus = await report.generateReport(req.params.username);
+
+    if(reportStatus === 'User not found') {
+        res.status(404).send("User Not Found");
+    }
+    else if(reportStatus === 'No Transactions Found') {
+        res.status(400).send('No Transactions');
+    }
+    else if(reportStatus) {
+        res.status(200).send(reportStatus);
+    }
+});
 
 export default userRouter;
