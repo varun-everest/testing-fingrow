82e73c17e7c62eab047e781ab991051dda4ccb4b
varun_everest <varunkumar.martha@everest.engineering>
2024-10-25 10:50:10 +0530

    Modify tag line

--- frontend/src/Components/Homepage/Homepage.tsx
+++ frontend/src/Components/Homepage/Homepage.tsx
@@ -5,7 +5,7 @@ const Homepage = () => {
   return (
     <div className='container'>
         <h1 className='title'>Welcome to FinGrow Finance Tracker Application</h1>
-        <h3 className='tag-title'>Smart Tracking for Smarter Transactions</h3>
+        <h3 className='tag-title'>Smart Tracking for Smarter Transactions!!</h3>
         <div>
             <Link to='/login'><button className='Hbutton'>Login</button></Link>
             <Link to='/register'><button className='Hbutton'>Register</button></Link>
