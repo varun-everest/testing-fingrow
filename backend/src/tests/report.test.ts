ad1b53cb9254863e82d6530d78cabe00d74ef791
varun_everest <varunkumar.martha@everest.engineering>
2024-10-25 11:26:02 +0530

    Modify readme

--- backend/src/tests/report.test.ts
+++
@@ -1,8 +0,0 @@
-import { generateReport } from "../controllers/Report"
-
-describe('Tests related to Financial Report', () => {
-
-    test('should generate Report', () => {
-        expect(generateReport()).toBeTruthy;
-    })
-})
--- backend/src/types/TIncome.ts
+++
@@ -1,6 +0,0 @@
-type TIncome = {
-    source : string,
-    amount : number,
-}
-
-export default TIncome;
